a86f26ad85e93361b7696f8c44478ab633c5bbb4
Stephen Strain <sfstrain@memphis.edu>
2016-11-03 21:04:42 -0500

    Added summary of last AIR meeting

---
+++ 161029_AIRmtg_CodeExample1.py
@@ -0,0 +1,7 @@
+Hello, it's Dr. Strain here. I'm going to show you how to run the first code example from our last AIR meeting. 
+
+1) If you have Ipython Notebook installed and working, just click the run button and it will run the code. Code cells in an Ipython Notebook are interactive, meaning you can make changes to them, click run, and immediately see the result. For instance, let's try changing "Obi-wan Kenobi" to "Old Ben Kenobi" and then click run.
+
+2) If you don't have Ipython Notebook (aka Jupyter) working yet, but you do have Ipython, you can use ipython's %paste code magic to copy the code in and run it. Type %magic from the IPython prompt to find out about IPython's many, highly useful code magics.
+
+3) Finally, you can copy the code into a text editor, and run it from the command line. 
