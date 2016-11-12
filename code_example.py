892e0320d6d1bc4c3b94d4ae9135cc6a694aa683
Stephen Strain <sfstrain@memphis.edu>
2016-11-12 10:57:41 -0600

    Update first tutorial, plus add new tutorial

---
+++ code_example.py
@@ -0,0 +1,12 @@
+heroes = ['Doctor Strange', 'Obi-Wan Kenobi', 
+            'Homer Simpson', 'Sarah Connor']
+
+villains = ['The Dread Dormammu', 'Darth Vader', 
+            'Mr. Burns', 'The T-1000 Terminator']
+
+for i in range(4):
+    print heroes[i],
+    print '   vs.   ',
+    print villains[i]
+    print "to the death!!!"
+    print
